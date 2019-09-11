21656f1d27d83af8c823770bc422415cbf0f3507
ISHAN PANDE <ishanpande@mail.usf.edu>
2019-09-10 20:56:59 -0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp2
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many years experience do you have in professional programming?");
+            string input = Console.ReadLine();
+            if (int.Parse(input) != 0)
+            {
+                for (int i = 0; i < (int.Parse(input)); i++)
+                {
+                    Console.WriteLine("You have" + input + "years of experience");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You have zero years of experience");
+            }
+        }
+    }
+}
